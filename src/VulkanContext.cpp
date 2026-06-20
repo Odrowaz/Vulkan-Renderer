f178895e9bb9887d29940a52446f1f0524110fc3
Federico De Felici <fdf8210@gmail.com>
2026-06-20 09:25:51 +0200

    fix CCW for model pipeline

--- src/VulkanContext.cpp
+++ src/VulkanContext.cpp
@@ -1148,7 +1148,7 @@ void VulkanContext::CreateModelPipeline()
     Rasterizer.polygonMode = VK_POLYGON_MODE_FILL;
     Rasterizer.lineWidth = 1.0f;
     Rasterizer.cullMode = VK_CULL_MODE_BACK_BIT;
-    Rasterizer.frontFace = VK_FRONT_FACE_CLOCKWISE;
+    Rasterizer.frontFace = VK_FRONT_FACE_COUNTER_CLOCKWISE;
 
     VkDynamicState DynamicStates[] = { VK_DYNAMIC_STATE_VIEWPORT, VK_DYNAMIC_STATE_SCISSOR }; 
     VkPipelineDynamicStateCreateInfo DynamicState{};
